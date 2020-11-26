f43a239c881e7ea121b41d1911f0affe908164cc
Takuya TAKAHASHI <taku.takahashi120@gmail.com>
2020-11-26 21:09:03 +0900

    fix arch

--- provision.sh
+++ provision.sh
@@ -2,6 +2,6 @@
 
 git clone https://github.com/takutakahashi/linux-desktop.git /tmp/linux-desktop
 cd /tmp/linux-desktop
-wget https://github.com/itamae-kitchen/mitamae/releases/download/v1.11.7/mitamae-aarch64-linux -O mitamae
+wget https://github.com/itamae-kitchen/mitamae/releases/download/v1.11.7/mitamae-x86_64-linux -O mitamae
 chmod +x mitamae
 ./mitamae local recipe/*
