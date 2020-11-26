ebce63ce063110572cdee1f8e3b0d8a49a9c2c5f
Takuya TAKAHASHI <taku.takahashi120@gmail.com>
2020-11-26 21:42:32 +0900

    mod alacritty path

--- recipe/alacritty.rb
+++ recipe/alacritty.rb
@@ -1,5 +1,5 @@
 execute "build alacritty" do
-  command "docker run -v #{ENV["HOME"]}/.dev/bin:/src takutakahashi/alacritty-build"
+  command "docker run -v /usr/local/bin:/src takutakahashi/alacritty-build"
   user "root"
-  not_if "ls #{ENV["HOME"]}/.dev/bin/alacritty"
+  not_if "ls /usr/local/bin/alacritty"
 end
