6d8a8f9401ac3fdccb73d28fe1a03188a844752b
Takuya TAKAHASHI <taku.takahashi120@gmail.com>
2020-11-26 21:37:27 +0900

    mod provision

--- provision.sh
+++ provision.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+rm -rf /tmp/linux-desktop
 git clone https://github.com/takutakahashi/linux-desktop.git /tmp/linux-desktop
 cd /tmp/linux-desktop
 wget https://github.com/itamae-kitchen/mitamae/releases/download/v1.11.7/mitamae-x86_64-linux -O mitamae
