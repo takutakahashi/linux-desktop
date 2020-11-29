450946977c7ae1f48cc7e225a5a48d6d174ea64b
Takuya TAKAHASHI <taku.takahashi120@gmail.com>
2020-11-30 01:20:35 +0900

    not if docker.sock is not found

--- recipe/root/alacritty.rb
+++ recipe/root/alacritty.rb
@@ -2,4 +2,5 @@ execute "build alacritty" do
   command "docker run -v /usr/local/bin:/src takutakahashi/alacritty-build"
   user "root"
   not_if "ls /usr/local/bin/alacritty"
+  only_if "ls /var/run/docker.sock"
 end
