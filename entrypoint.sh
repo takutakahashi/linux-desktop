4872ad4cc6c14b625c358be81985ba410c920954
Takuya TAKAHASHI <taku.takahashi120@gmail.com>
2020-11-30 00:56:23 +0900

    .rb

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,5 @@
 
 wget https://github.com/itamae-kitchen/mitamae/releases/download/v1.11.7/mitamae-x86_64-linux -O mitamae
 chmod +x mitamae
-sudo ./mitamae local recipe/root/*
-./mitamae local recipe/user/*
+sudo ./mitamae local recipe/root/*.rb
+./mitamae local recipe/user/*.rb
