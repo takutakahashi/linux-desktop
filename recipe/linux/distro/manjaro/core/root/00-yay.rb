8920695501cfb336330f619cc31cbf794aeff98d
Takuya TAKAHASHI <taku.takahashi120@gmail.com>
2022-08-17 12:00:07 +0900

    mv to ubuntu

---
+++ recipe/linux/distro/manjaro/core/root/00-yay.rb
@@ -0,0 +1,9 @@
+package 'git' do
+  action :install
+  not_if 'git version'
+end
+
+execute "install yay" do
+  command "git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si && cd .. && rm -rf yay"
+  not_if "which yay"
+end
