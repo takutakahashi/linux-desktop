81e17da9c4adb51ee48b1548e00fdc7bb9f81884
Takuya TAKAHASHI <taku.takahashi120@gmail.com>
2022-08-17 15:26:13 +0900

    use local user

--- recipe/linux/distro/manjaro/core/root/00-yay.rb
+++ recipe/linux/distro/manjaro/core/root/00-yay.rb
@@ -4,6 +4,7 @@ package 'git' do
 end
 
 execute "install yay" do
+  user "owner"
   command "git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si && cd .. && rm -rf yay"
   not_if "which yay"
 end
