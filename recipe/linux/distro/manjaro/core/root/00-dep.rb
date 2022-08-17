9683a527ac6bf5fe95f662ad4f3a008d5df3b960
Takuya TAKAHASHI <taku.takahashi120@gmail.com>
2022-08-17 17:38:50 +0900

    add yay and pacman

--- recipe/linux/distro/manjaro/core/root/00-dep.rb
+++ recipe/linux/distro/manjaro/core/root/00-dep.rb
@@ -2,3 +2,9 @@ package 'git' do
   action :install
   not_if 'git version'
 end
+
+f = File.open("pacmanfile")
+execute "install pacman" do
+  command "pacman --needed --noconfirm -S #{f.read}"
+end
+
--- recipe/linux/distro/manjaro/core/user/install.rb
+++ recipe/linux/distro/manjaro/core/user/install.rb
@@ -2,12 +2,11 @@ execute "update database" do
   command "yay -Sy"
 end
 
-package_list_native = `pacman -Qqen`
 package_list_yay = `pacman -Qqem`
 f = File.open("yayfile")
 f.each_line do |p|
   p.chomp!
-  if !(package_list_native.include?(p) || package_list_yay.include?(p))
+  if !package_list_yay.include?(p)
     execute p do
       command "yes |yay --noprovides --answerdiff None --answerclean None --mflags '--noconfirm' -S #{p}"
     end
