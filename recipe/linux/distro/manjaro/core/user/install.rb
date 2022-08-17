0949f27db5490dc885368a9130dac294b84a9bed
Takuya TAKAHASHI <taku.takahashi120@gmail.com>
2022-08-17 17:53:25 +0900

    Revert "restore from yayfile"
    This reverts commit 746fbc1b716af6b97f457d66cee6beef302cf765.

--- recipe/linux/distro/manjaro/core/user/install.rb
+++ recipe/linux/distro/manjaro/core/user/install.rb
@@ -4,8 +4,11 @@ end
 
 package_list_yay = `pacman -Qqem`
 f = File.open("yayfile")
-if package_list_yay != f.read
-  execute "restore yayfile" do
-    command "yay --needed --noconfirm --removemake -S - < yayfile"
+f.each_line do |p|
+  p.chomp!
+  if !package_list_yay.include?(p)
+    execute p do
+      command "yes |yay --noprovides --answerdiff None --answerclean None --mflags '--noconfirm' -S #{p}"
+    end
   end
 end
