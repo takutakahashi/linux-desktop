746fbc1b716af6b97f457d66cee6beef302cf765
Takuya TAKAHASHI <taku.takahashi120@gmail.com>
2022-08-17 17:49:11 +0900

    restore from yayfile

--- recipe/linux/distro/manjaro/core/user/install.rb
+++ recipe/linux/distro/manjaro/core/user/install.rb
@@ -4,11 +4,8 @@ end
 
 package_list_yay = `pacman -Qqem`
 f = File.open("yayfile")
-f.each_line do |p|
-  p.chomp!
-  if !package_list_yay.include?(p)
-    execute p do
-      command "yes |yay --noprovides --answerdiff None --answerclean None --mflags '--noconfirm' -S #{p}"
-    end
+if package_list_yay != f.read
+  execute "restore yayfile" do
+    command "yay --needed --noconfirm --removemake -S - < yayfile"
   end
 end
