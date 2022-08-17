cce655abf7b00fe49cb5ac2c4648e9318a67f018
Takuya TAKAHASHI <taku.takahashi120@gmail.com>
2022-08-17 14:39:49 +0900

    add installation

--- recipe/linux/distro/manjaro/core/root/install.rb
+++ recipe/linux/distro/manjaro/core/root/install.rb
@@ -2,7 +2,10 @@ package_list_native = `pacman -Qqen`
 package_list_yay = `pacman -Qqem`
 f = File.open("yayfile")
 f.each_line do |p|
+  p.chomp!
   if !(package_list_native.include?(p) || package_list_yay.include?(p))
-    puts p
+    execute p do
+      command "yes |yay --noprovides --answerdiff None --answerclean None --mflags '--noconfirm' -S #{p}"
+    end
   end
 end
