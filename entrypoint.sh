75e5ba153eb1a7cbad374a0fa80d1fc989ed4508
Takuya TAKAHASHI <taku.takahashi120@gmail.com>
2022-08-17 14:55:27 +0900

    add make manjaro

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,21 +4,7 @@ OS=`uname |tr '[A-Z]' '[a-z]'`
 if [[ "$ARCH" = "arm64" ]]; then
   ARCH="aarch64"
 fi
+DISTRO=$1
 wget https://github.com/itamae-kitchen/mitamae/releases/download/v1.12.8/mitamae-${ARCH}-${OS} -O mitamae
 chmod +x mitamae
-sudo ./mitamae local recipe/core/root/*.rb ||true
-./mitamae local recipe/core/user/*.rb
-# for wsl
-#if [[ "$WSL_DISTRO_NAME" != "" ]]; then
-#fi
-
-# for Linux Desktop
-
-if [[ "$OS" = "linux" ]]; then
-  sudo ./mitamae local recipe/linux/core/root/*.rb
-  ./mitamae local recipe/linux/core/user/*.rb
-  if [[ "$XDG_CURRENT_DESKTOP" != "" ]]; then
-    sudo ./mitamae local recipe/linux/desktop/root/*.rb
-    ./mitamae local recipe/linux/desktop/user/*.rb
-  fi
-fi
+./${DISTRO}.sh
---
+++ manjaro.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo ./mitamae_local.sh recipe/linux/core/root/
+./mitamae_local.sh recipe/linux/core/user/
+sudo ./mitamae_local.sh recipe/linux/distro/manjaro/core/root/
