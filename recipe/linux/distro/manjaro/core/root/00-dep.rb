648a17e126df3a509870a8657de894f381a25f63
Takuya TAKAHASHI <taku.takahashi120@gmail.com>
2022-08-17 17:55:32 +0900

    update pacman update

--- recipe/linux/distro/manjaro/core/root/00-dep.rb
+++ recipe/linux/distro/manjaro/core/root/00-dep.rb
@@ -3,6 +3,10 @@ package 'git' do
   not_if 'git version'
 end
 
+execute "update database" do
+  command "pacman -Sy"
+end
+
 execute "install pacman" do
   command "pacman -S --needed --noconfirm $(comm -12 <(pacman -Slq | sort) <(sort pacmanfile))"
 end
