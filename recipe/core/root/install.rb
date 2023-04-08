2761e205a4705082c20ff6e0626d950ff6355aa6
Takuya TAKAHASHI <taku.takahashi120@gmail.com>
2023-04-09 01:59:18 +0900

    add doppler

---
+++ recipe/core/root/install.rb
@@ -0,0 +1,5 @@
+execute "install doppler" do
+  command '(curl -Ls --tlsv1.2 --proto "=https" --retry 3 https://cli.doppler.com/install.sh || wget -t 3 -qO- https://cli.doppler.com/install.sh) | sh'
+  not_if "which doppler"
+end
+
