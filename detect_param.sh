b90a68c2098a0aa1efa9c6ac2035de4e27b176be
Takuya TAKAHASHI <taku.takahashi120@gmail.com>
2023-04-09 14:54:55 +0900

    add gnome

--- detect_param.sh
+++ detect_param.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 uname -a |grep -i manjaro >/dev/null && DISTRO=manjaro
-which sway >/dev/null && DESKTOP=sway
-
+which sway >/dev/null 2>&1 && DESKTOP="$DESKTOP sway"
+which gnome-shell >/dev/null 2>&1 && DESKTOP="$DESKTOP gnome"
 echo $DISTRO $DESKTOP
