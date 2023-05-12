a146dd47a416d18e4c02337d98f1b3cb61ca1a08
Takuya TAKAHASHI <taku.takahashi120@gmail.com>
2023-05-12 09:45:32 +0900

    rm dust

--- recipe/linux/distro/ubuntu/core/user/brew_package.rb
+++ recipe/linux/distro/ubuntu/core/user/brew_package.rb
@@ -1,13 +1,6 @@
 brew_path = "#{ENV["HOME"]}/.linuxbrew/bin/brew"
 %w(
   htop
-  ghq
-  peco
-  kubectx
-  kubectl
-  anyenv
-  direnv
-  stern
 ).each do |p|
   execute p do
     command "#{brew_path} install #{p}"
