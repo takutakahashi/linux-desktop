ec4cb640b8cfc6705fede4f3ffc1c93f9de9657e
Takuya TAKAHASHI <taku.takahashi120@gmail.com>
2023-05-23 16:56:14 +0900

    fix package

--- recipe/core/root/install.rb
+++ recipe/core/root/install.rb
@@ -1,8 +1,4 @@
-execute "install doppler" do
-  command '(curl -Ls --tlsv1.2 --proto "=https" --retry 3 https://cli.doppler.com/install.sh || wget -t 3 -qO- https://cli.doppler.com/install.sh) | sh'
-  not_if "which doppler"
-end
-
 package "unzip" do
   action :install
+  not_if "which unzip"
 end
